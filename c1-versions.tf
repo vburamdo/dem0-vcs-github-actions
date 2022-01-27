6b9eb21be214a45656be67b849fe538b2cf6d59f
vinod <vinod.buram@yahoo.com>
2022-01-27 07:49:15 +0000

    hlsjdf

--- c1-versions.tf
+++ c1-versions.tf
@@ -13,6 +13,7 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
