70749c4a4e8ade12aa56ab72cc502dcba167981a
vburamdo <vinod.buram@yahoo.com>
2022-01-27 13:53:10 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -12,6 +12,8 @@ terraform {
 # Provider Block
 provider "aws" {
   region = var.aws_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
